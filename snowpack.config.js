9514f7cd17e908286c8ad019909ed7ce91b43f09
jose_nicolas_aristizabal_ramirez <josenicolasaristizabalramirez@gmail.com>
2023-11-11 21:48:45 -0500

    update snowpack.config.js

--- snowpack.config.js
+++ snowpack.config.js
@@ -5,6 +5,6 @@ module.exports = {
     src: '/_dist_',
   },
   buildOptions: {
-    baseUrl: 'https://nicolas1996-ing.github.io//avocate-html',
+    baseUrl: 'https://nicolas1996-ing.github.io/avocate-html',
   },
 }
